b4f98b2dd54922eed16b900695e5fd58545feb69
ntereshchenko <nastya.tereschenko02@gmail.com>
2023-11-16 21:53:06 +0200

    Lab0: Creation of Hello World task

--- src/main/java/edu/ntudp/fit/lab0/Main.java
+++ src/main/java/edu/ntudp/fit/lab0/Main.java
@@ -1,4 +1,4 @@
-package edu.ntudp.fit;public class Main {
+package edu.ntudp.fit.lab0;public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
