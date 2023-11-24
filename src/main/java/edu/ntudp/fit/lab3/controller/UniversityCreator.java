762cd2575225eb90f9cceb7bfbe3acf0187da9bf
ntereshchenko <nastya.tereschenko02@gmail.com>
2023-11-24 14:42:41 +0200

    Lab0: Creation of Hello World task

--- src/main/java/edu/ntudp/fit/lab3/controller/UniversityCreator.java
+++ src/main/java/edu/ntudp/fit/lab3/controller/UniversityCreator.java
@@ -4,11 +4,9 @@ import edu.ntudp.fit.lab3.Sex;
 import edu.ntudp.fit.lab3.models.*;
 
 
-import java.util.ArrayList;
+
 import java.util.List;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class UniversityCreator {
 
@@ -28,33 +26,33 @@ public class UniversityCreator {
 
     public University createTypicalUniversity() {
 
-        Human newHuman1 = humanCreate.createHuman("Bondarenko", "Paulo", "Ivanovych", Sex.MALE);
-        Human newHuman2 = humanCreate.createHuman("Tereshchenko", "Nastya", "Sergeevna", Sex.FEMALE);
-        Human newHuman3 = humanCreate.createHuman("Matveev", "Max", "Olegovich", Sex.MALE);
-        Human newHuman4 = humanCreate.createHuman("Maykov", "Oleg", "Antonovich", Sex.MALE);
-        Human newHuman5 = humanCreate.createHuman("Romanenko", "Ira", "Andreevna", Sex.FEMALE);
-        Human newHuman6 = humanCreate.createHuman("Gor", "Alex", "Olegovich", Sex.MALE);
+        Human firstHuman = humanCreate.createHuman("Bondarenko", "Paulo", "Ivanovych", Sex.MALE);
+        Human secondHuma = humanCreate.createHuman("Tereshchenko", "Nastya", "Sergeevna", Sex.FEMALE);
+        Human thirdHuman = humanCreate.createHuman("Matveev", "Max", "Olegovich", Sex.MALE);
+        Human fourHuman = humanCreate.createHuman("Maykov", "Oleg", "Antonovich", Sex.MALE);
+        Human fiveHuman = humanCreate.createHuman("Romanenko", "Ira", "Andreevna", Sex.FEMALE);
+        Human sixHuman = humanCreate.createHuman("Gor", "Alex", "Olegovich", Sex.MALE);
 
-        Student newStudent1 = studentCreate.createStudent(newHuman1);
-        Student newStudent2 = studentCreate.createStudent(newHuman2);
-        Student newStudent3 = studentCreate.createStudent(newHuman3);
-        Student newStudent4 = studentCreate.createStudent(newHuman4);
-        Student newStudent5 = studentCreate.createStudent(newHuman5);
-        Student newStudent6 = studentCreate.createStudent(newHuman6);
-
-
-        Group newGroup1 = createGroup("122-21-sk", newHuman2, newStudent1, newStudent2, newStudent3);
-        Group newGroup2 = createGroup("122-21-1", newHuman6, newStudent4, newStudent5, newStudent6);
-
-        Department newDepartment1 = createDepartment("PZKS", newHuman1, newGroup1, newGroup2);
+        Student firstStudent = studentCreate.createStudent(firstHuman);
+        Student secondStudent = studentCreate.createStudent(secondHuma);
+        Student  thirdStudent = studentCreate.createStudent(thirdHuman);
+        Student fourStudent = studentCreate.createStudent(fourHuman);
+        Student fiveStudent = studentCreate.createStudent(fiveHuman);
+        Student sixStudent = studentCreate.createStudent(sixHuman);
 
 
-        Faculty newFaculty1 = createFaculty("FIT", newHuman5, newDepartment1);
+        Group firstroup = createGroup("122-21-sk", secondHuma, firstStudent, secondStudent,  thirdStudent);
+        Group secondGroup = createGroup("122-21-1", sixHuman, fourStudent, fiveStudent, sixStudent);
+
+        Department firstDepartment = createDepartment("PZKS", firstHuman, firstroup, secondGroup);
 
 
-        University newUniversity1 = createUniversity("NTU DP", newHuman4, List.of(newFaculty1));
+        Faculty firstFaculty = createFaculty("FIT", fiveHuman, firstDepartment);
 
-        return newUniversity1;
+
+        University university = createUniversity("NTU DP", fourHuman, List.of(firstFaculty));
+
+        return university;
     }
 
     private Group createGroup(String groupCode, Human groupLeader, Student... students) {
--- src/main/java/edu/ntudp/fit/lab3/lr3/Run.java
+++ src/main/java/edu/ntudp/fit/lab3/lr3/Run.java
@@ -1,11 +1,6 @@
 package edu.ntudp.fit.lab3.lr3;
-
 import edu.ntudp.fit.lab3.controller.*;
-import edu.ntudp.fit.lab3.models.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 
 public class Run {
 
